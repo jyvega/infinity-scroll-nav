1a45f20d46697647da63b05e93999f7a9930a60c
Jesus Vega <jesus.vega@devmente.com>
2022-11-01 09:08:00 -0400

    UPD: card styles UPD: background-image for header and nav ADD: infinity scroll ADD: load data from external api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,16 +10,36 @@ import {Product} from "./share/interfaces";
 export class AppComponent implements OnInit {
   title = 'infinity-scroll-nav';
   products: Product[] = [];
+  offset: number = 0;
+  limit: number = 8;
 
   constructor(private httpClient: HttpClient) {
 
   }
 
   ngOnInit(): void {
-    this.httpClient.get('https://api.escuelajs.co/api/v1/products')
+    this.products = [];
+    this.getProducts();
+    addEventListener('scroll', () => {
+      if (Math.trunc(window.scrollY) == document.documentElement.scrollHeight - window.innerHeight) {
+        this.getProducts();
+      }
+    }, {passive: true});
+  }
+
+  getProducts() {
+    this.httpClient.get('https://api.escuelajs.co/api/v1/products', {
+      params: {
+        offset: this.offset,
+        limit: this.limit
+      }
+    })
       .toPromise()
       .then(value => {
-        this.products = !!value ? value as Array<Product> : []
+        this.products.push(...value as Array<Product>);
+        if (this.products.length > 0) {
+          this.offset += this.limit;
+        }
       })
       .catch(reason => console.log(reason))
   }
