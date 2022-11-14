219fe7edc1582993d57122bc51b8693a86604a46
Jesus Vega <jesus.vega@devmente.com>
2022-11-13 19:46:05 -0500

    fix: code smells

--- src/app/share/components/card/card.component.ts
+++ src/app/share/components/card/card.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {Product} from "../../interfaces";
 
 @Component({
@@ -6,13 +6,6 @@ import {Product} from "../../interfaces";
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css']
 })
-export class CardComponent implements OnInit {
+export class CardComponent {
   @Input() product!: Product;
-
-  constructor() {
-  }
-
-  ngOnInit(): void {
-  }
-
 }
--- src/app/share/components/nav/nav.component.ts
+++ src/app/share/components/nav/nav.component.ts
@@ -1,15 +1,10 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
-export class NavComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class NavComponent {
 
 }
